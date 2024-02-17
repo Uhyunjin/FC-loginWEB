1bec12cf99dceebb0c5aa59b373ca306e97cce5d
uhyunjin <uhyeonjin0619@gmail.com>
2024-02-17 23:08:08 +0900

    exception, null pointer exception 예외처리 하기

--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -1,22 +1,35 @@
 package com.fastcampus.ch2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExceptionController {
+	
+	@ExceptionHandler(NullPointerException.class)
+	public String catcher2(Exception ex, Model m) {
+		m.addAttribute("ex", ex);
+		return "error";
+	}
+	@ExceptionHandler(Exception.class)
+	public String catcher(Exception ex, Model m) {
+		m.addAttribute("ex", ex);
+		return "error";
+	}
+	//메서드가 여러개일 때 try-catch 코드가 중복되므로 별도로 예외처리 메서드를 추가해준다+exceptionhandler어노테이션
+	
+	
 	@RequestMapping("/ex")
 	public String main() throws Exception{
-		try {
 			throw new Exception("예외가 발생했습니다.");
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			
-			return "error";
-		}
-		
-		//return "index";
+			//항상 예외가 발생
+	}
+	@RequestMapping("/ex2")
+	public String main2() throws Exception{
+			throw new NullPointerException("예외가 발생했습니다.");
+			//NullPointerException 발생
 	}
 
 }
