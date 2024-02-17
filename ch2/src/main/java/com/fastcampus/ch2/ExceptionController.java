86b2ef41650ca3e2f24147afa98b7d20362402d1
uhyunjin <uhyeonjin0619@gmail.com>
2024-02-17 23:45:05 +0900

    ControllerAdvice를 사용해서 GlobalCatcher(공통 예외처리 클래스)만들기

--- ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
+++ ch2/src/main/java/com/fastcampus/ch2/ExceptionController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ExceptionController {
 	
-	@ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
-	// 여러개의 예외를 하나의 클래스에 담을 땐 맵 형태로 받는다
-	public String catcher2(Exception ex, Model m) {
-		m.addAttribute("ex", ex);
-		return "error";
-	}
+//	@ExceptionHandler({NullPointerException.class, FileNotFoundException.class})
+//	// 여러개의 예외를 하나의 클래스에 담을 땐 맵 형태로 받는다
+//	public String catcher2(Exception ex, Model m) {
+//		m.addAttribute("ex", ex);
+//		return "error";
+//	}
 //	@ExceptionHandler(Exception.class)
 //	public String catcher(Exception ex, Model m) {
 //		m.addAttribute("ex", ex);
